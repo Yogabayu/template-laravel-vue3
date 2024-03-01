4e2905833c6826a99a345ea15a41adb61070708f
Yogabayu <yogabayusbi@gmail.com>
2024-03-01 13:39:26 +0700

    v2.1-kerinci | feat: update

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const mainURL = axios.create({
   // baseURL: 'http://localhost:8000/api/v1',
   baseURL: 'https://templatevue.yogabayuap.com/api/v1',
-  timeout: 5000, 
+  // timeout: 5000, 
 });
 // Interceptor untuk menambahkan token ke header Authorization
 mainURL.interceptors.request.use(
