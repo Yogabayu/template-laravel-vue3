dfcfc2eb2441a530f695555ff8ef58c68f2688b6
Yogabayu <yogabayusbi@gmail.com>
2024-03-01 11:10:14 +0700

    v2.1-kerinci | fix: update bug

--- database/migrations/2024_02_26_031205_add_dev_name_to_deviceverifications.php
+++ database/migrations/2024_02_26_031205_add_dev_name_to_deviceverifications.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('deviceverifications', function (Blueprint $table) {
+        Schema::table('deviceVerifications', function (Blueprint $table) {
             $table->string('nameDev')->nullable();
         });
     }
