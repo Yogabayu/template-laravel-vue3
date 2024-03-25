d8e123915aca5a1323a89bd36edc51b6e85ca149
Yogabayu <yogabayusbi@gmail.com>
2024-03-25 16:02:31 +0700

    fix

--- database/seeders/DraftApprovalLevelSeeder.php
+++ database/seeders/DraftApprovalLevelSeeder.php
@@ -15,8 +15,7 @@ class DraftApprovalLevelSeeder extends Seeder
     {
         $positions = [
             ['id' => 1, 'name' => 'Level 1', 'desc' => 'normal', 'canUpload' => 0, 'canEdit' => 0, 'canComment' => 0],
-            ['id' => 2, 'name' => 'Level 2', 'desc' => 'uploader', 'canUpload' => 1, 'canEdit' => 1, 'canComment' => 1],
-            ['id' => 3, 'name' => 'Level 3', 'desc' => 'reviewer', 'canUpload' => 1, 'canEdit' => 1, 'canComment' => 1],
+            ['id' => 2, 'name' => 'Level 2', 'desc' => 'uploader/reviewer', 'canUpload' => 1, 'canEdit' => 1, 'canComment' => 1],
         ];
 
         foreach ($positions as $position) {
